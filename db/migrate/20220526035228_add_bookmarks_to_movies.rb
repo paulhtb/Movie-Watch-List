e0493491e34fa9c946e76533d993a7a1c39735ef
Paul Hattabe <hattabe.p@gmail.com>
2022-05-26 11:59:24 +0800

    Created the DB tables for Lists, Movies and Bookmarks

---
+++ db/migrate/20220526035228_add_bookmarks_to_movies.rb
@@ -0,0 +1,5 @@
+class AddBookmarksToMovies < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :movies, :bookmarks, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_05_26_035824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "bookmarks_id", null: false
+    t.index ["bookmarks_id"], name: "index_lists_on_bookmarks_id"
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "overview"
+    t.string "poster_url"
+    t.float "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "bookmarks_id", null: false
+    t.index ["bookmarks_id"], name: "index_movies_on_bookmarks_id"
+  end
+
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
+  add_foreign_key "lists", "bookmarks", column: "bookmarks_id"
+  add_foreign_key "movies", "bookmarks", column: "bookmarks_id"
 end
